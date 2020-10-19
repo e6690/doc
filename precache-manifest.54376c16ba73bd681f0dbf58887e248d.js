bd839ab9ba71a065be5997ec598526a7546b3530
elysee eve <elysee.eve@codeloccol.org>
2020-10-19 08:04:46 +0100

    Updates

---
+++ precache-manifest.54376c16ba73bd681f0dbf58887e248d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe3decf550976aec4bd25ebe441b3e1f",
+    "url": "/shortcut/index.html"
+  },
+  {
+    "revision": "73803be39adc93742787",
+    "url": "/shortcut/static/css/main.32c95d58.chunk.css"
+  },
+  {
+    "revision": "9ffd7671d1e401e7758f",
+    "url": "/shortcut/static/js/2.022829d0.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/shortcut/static/js/2.022829d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73803be39adc93742787",
+    "url": "/shortcut/static/js/main.2b70fd78.chunk.js"
+  },
+  {
+    "revision": "929d729a885eff8ab476",
+    "url": "/shortcut/static/js/runtime-main.397d4e65.js"
+  },
+  {
+    "revision": "ffb04a4e5580cf17b9d2e549c1894bb9",
+    "url": "/shortcut/static/media/Ibrahim Yacouba.ffb04a4e.jpg"
+  },
+  {
+    "revision": "fac507f3f816eed223453387a5b01444",
+    "url": "/shortcut/static/media/Mahamadou Issoufou.fac507f3.jpg"
+  },
+  {
+    "revision": "6e20beeff322cbc2b6ce8c038f53835f",
+    "url": "/shortcut/static/media/Mahamane Ousmane .6e20beef.jpg"
+  },
+  {
+    "revision": "93bc037e28439f0f32e9c51d8f613d18",
+    "url": "/shortcut/static/media/Salou Djibo .93bc037e.jpg"
+  },
+  {
+    "revision": "41d3855445466e6f1eadef14cca1af1b",
+    "url": "/shortcut/static/media/Seyni Oumarou.41d38554.jpg"
+  },
+  {
+    "revision": "45db901f391c0a4898b1c62ce33b2710",
+    "url": "/shortcut/static/media/jpg.45db901f.jpeg"
+  },
+  {
+    "revision": "011950c48602cfbe1b3ded86668d930b",
+    "url": "/shortcut/static/media/mnrd.011950c4.jpeg"
+  },
+  {
+    "revision": "c3738967791722a23c29e241ad7d3e34",
+    "url": "/shortcut/static/media/mnsd.c3738967.png"
+  },
+  {
+    "revision": "df813ef26d3c05f233fbc75d7dd526fa",
+    "url": "/shortcut/static/media/mpn.df813ef2.png"
+  },
+  {
+    "revision": "3514f54efc60e5531c7870abbcb8728c",
+    "url": "/shortcut/static/media/pnds.3514f54e.jpeg"
+  }
+]);
